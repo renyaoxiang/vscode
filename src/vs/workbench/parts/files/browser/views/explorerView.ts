93d376060d5a2b3cb3ed389b0c89e60511bf07c9
Sandeep Somavarapu <sasomava@microsoft.com>
2017-06-06 22:21:22 +0200

    Debt: Composed views viewlet - Implement a viewlet composed of views - Adopt Explorer viewlet to extend Composed views viewlet

--- src/vs/workbench/parts/files/browser/views/explorerView.ts
+++ src/vs/workbench/parts/files/browser/views/explorerView.ts
@@ -46,6 +46,11 @@ import { IEnvironmentService } from 'vs/platform/environment/common/environment'
 import { attachListStyler } from 'vs/platform/theme/common/styler';
 import { IViewOptions } from 'vs/workbench/parts/views/browser/viewsRegistry';
 
+export interface IExplorerViewOptions extends IViewOptions {
+	settings: any;
+	viewletState: FileViewletState;
+}
+
 export class ExplorerView extends CollapsibleViewletView {
 
 	public static ID: string = 'workbench.explorer.fileView';
@@ -80,10 +85,8 @@ export class ExplorerView extends CollapsibleViewletView {
 	private settings: any;
 
 	constructor(
-		viewletState: FileViewletState,
-		options: IViewOptions,
-		settings: any,
-		headerSize: number,
+		id: string,
+		options: IExplorerViewOptions,
 		@IMessageService messageService: IMessageService,
 		@IContextMenuService contextMenuService: IContextMenuService,
 		@IInstantiationService private instantiationService: IInstantiationService,
@@ -100,10 +103,10 @@ export class ExplorerView extends CollapsibleViewletView {
 		@IWorkbenchThemeService private themeService: IWorkbenchThemeService,
 		@IEnvironmentService private environmentService: IEnvironmentService
 	) {
-		super(options.actionRunner, options.collapsed, nls.localize('explorerSection', "Files Explorer Section"), messageService, keybindingService, contextMenuService, headerSize);
+		super(options.actionRunner, options.collapsed, nls.localize('explorerSection', "Files Explorer Section"), messageService, keybindingService, contextMenuService);
 
-		this.settings = settings;
-		this.viewletState = viewletState;
+		this.settings = options.settings;
+		this.viewletState = options.viewletState;
 		this.actionRunner = options.actionRunner;
 		this.autoReveal = true;
 
